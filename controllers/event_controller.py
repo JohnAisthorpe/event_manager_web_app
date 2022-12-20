a310f1cb5123228d7f258d0c356d08c6a38fef20
John Aisthorpe <john.aisthorpe@gmail.com>
2022-12-20 19:51:38 +0000

    participation/event controllers almost done

---
+++ controllers/event_controller.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.event import Event
+import repositories.event_repository as event_repository
+import repositories.athlete_repository as athlete_repository
+
+event_blueprint = Blueprint("/events", __name__)
+
+@event_blueprint.route("/events")
+def events():
+    events = event_repository.select_all()
+    return render_template("events/index.html", events = events)
+
+@event_blueprint.route("/events/<id>")
+def show(id):
+    event = event_repository.select(id)
+    athlete = athlete_repository.event_participation(event)
+    return render_template("events/show.html", event = event, athlete = athlete)
---
+++ controllers/participation controller.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template,request, redirect
+from flask import Blueprint
+from models.participation import Participation
+import repositories.participation_repository as participation_repository 
+import repositories.athlete_repository as athlete_repository 
+import repositories.event_repository as event_repository
+
+participation_blueprint = Blueprint("/participation", __name__)
+
+@participation_blueprint.route("/participation")
+def participation():
+    participation = participation_repository.select_all()
+    return render_template("participation/index.html", participation = participation)
+
+@participation 
