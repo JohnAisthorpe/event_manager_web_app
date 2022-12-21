446adbf366b485789f93ece18d9656ad06ced617
John Aisthorpe <john.aisthorpe@gmail.com>
2022-12-21 11:09:24 +0000

    from for new athlete created

--- controllers/athlete_controller.py
+++ controllers/athlete_controller.py
@@ -9,13 +9,13 @@ athlete_blueprint = Blueprint("athletes", __name__)
 @athlete_blueprint.route("/athletes")
 def athletes():
     athletes = athlete_repository.select_all()
-    return render_template("athlete/index.html", athletes = athletes)
+    return render_template("athletes/index.html", athletes = athletes)
 
 @athlete_blueprint.route("/athlete/<id>")
 def show (id):
     athlete = athlete_repository.select(id)
     events = event_repository.event_participation(athlete)
-    return render_template('athlete/show.html', athlete = athlete)
+    return render_template('athletes/show.html', athlete = athlete)
 
 # new athlete form
 @athlete_blueprint.route("/athletes/new", methods = ['GET'])
@@ -27,7 +27,17 @@ def new_athlete():
 # new athlete submit 
 @athlete_blueprint.route("/athletes", methods=['POST'])
 def create_athlete():
-    athletes = athlete_repository.select_all()
-    events = event_repository.select_all
-    return render_template("athletes/new.html", athletes = athletes)
+    
+    # Extract the request data
+    name = request.form('name')
+
+    # Create a new athlete object
+    athlete = Athlete(name)
+
+    # Add the athlete to the database
+    athlete_repository.save(athlete)
+
+    # Return a response with the new athlete's information
+    return redirect('/athletes')
+
 
